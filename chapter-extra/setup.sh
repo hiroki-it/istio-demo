d7bcd1aabc8d972f76ec25f2192ace156623a8bc
hiroki-it <hasegawafeedshop@gmail.com>
2026-06-07 16:59:56 +0900

    セットアップ手順を修正した

--- chapter-extra/setup.sh
+++ chapter-extra/setup.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-echo "Starting setup for Appendix 2..."
+echo "Starting setup for Chapter Extra..."
 
 # MySQLコンテナの作成
 echo "Creating MySQL container..."
@@ -10,7 +10,7 @@ docker compose -f databases/docker-compose.yaml up -d
 
 # Namespaceの作成
 echo "Creating Namespace..."
-kubectl apply -f chapter-extra/shared/namespace.yaml
+kubectl apply --server-side -f chapter-extra/shared/namespace.yaml
 
 # Bookinfoアプリケーションの作成
 echo "Deploying Bookinfo application..."
@@ -24,19 +24,12 @@ echo "Deploying Istiod control plane..."
 helmfile -f chapter-extra/istio/istio-base/helmfile.yaml apply
 helmfile -f chapter-extra/istio/istio-istiod/helmfile.yaml apply
 
-# Istio IngressGatewayの削除
-echo "Deleting Istio IngressGateway..."
-kubectl delete deployment istio-ingressgateway -n istio-ingress
-kubectl delete service istio-ingressgateway -n istio-ingress
-
-# Gateway APIのカスタムリソース定義の作成
-echo "Creating Gateway API CRDs..."
-CRD_VERSION=1.3.0
-kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v${CRD_VERSION}/standard-install.yaml
-
-# IstioのL4/L7トラフィック管理系リソースをGateway APIリソースに置き換え
-echo "Replacing Istio traffic management resources with Gateway API resources..."
+# Istio IngressGatewayの作成
+echo "Deploying Istio IngressGateway..."
 helmfile -f chapter-extra/istio/istio-ingress/helmfile.yaml apply
+
+# Istioリソースの作成
+echo "Creating Istio resources..."
 helmfile -f chapter-extra/bookinfo-app/details-istio/helmfile.yaml apply
 helmfile -f chapter-extra/bookinfo-app/productpage-istio/helmfile.yaml apply
 helmfile -f chapter-extra/bookinfo-app/ratings-istio/helmfile.yaml apply
@@ -46,5 +39,13 @@ helmfile -f chapter-extra/bookinfo-app/reviews-istio/helmfile.yaml apply
 echo "Rolling out Kubernetes Pods..."
 kubectl rollout restart deployment -n bookinfo
 
-echo "Setup Extra 2 completed successfully!"
+# Prometheusの作成
+echo "Deploying Prometheus..."
+helmfile -f chapter-extra/prometheus/helmfile.yaml apply
+
+# Kialiの作成
+echo "Deploying Kiali..."
+helmfile -f chapter-extra/kiali/helmfile.yaml apply
+
+echo "Setup Chapter Extra completed successfully!"
 exit 0
